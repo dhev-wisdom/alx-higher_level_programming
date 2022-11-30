1877687d1368bf7d0f14fc8a50044dca80e246f0
dhev-wisdom <nonsowisdom62@gmail.com>
2022-11-30 11:23:28 -0800

    chmod

---
+++ 0x00-python-hello_world/test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, "equals", x, "*", n//x)
+            break
+    else:
+        print(n, "is a prime number")
+
+g = "Hello"
+match g:
+    case "Hello":
+        print("Hello world")
+    case other:
+        print("no match found")
