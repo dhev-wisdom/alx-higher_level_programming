7e1f9bffbec10657f0e9614aa3713fe00d652ec4
dhev-wisdom <nonsowisdom62@gmail.com>
2022-12-01 06:29:53 -0800

    hex()

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for lettersq in range(97, 123):
     if lettersq != 101 and lettersq != 113:
-        print(chr(lettersq), end="")
+       # print(chr(lettersq), end="")
+        print(*"{}".format(chr(lettersq)))
