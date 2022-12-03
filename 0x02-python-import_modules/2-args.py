be26f48c95f01d7689622ac66bb08666a6093ec0
dhev-wisdom <nonsowisdom62@gmail.com>
2022-12-03 13:31:40 -0800

    correct argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-from sys import argv
-print("{:d} {:s}".format(len(argv) - 1, "argument" if len(argv) == 2 else "arguments"))
-if len(argv) > 1:
-    for arg in range(1:len(argv)):
-        print("{:d}: {:s}".format(arg, str(argv[arg]))
+import sys
+print("{:d} {:s}".format(len(sys.argv) - 1, "argument" if len(sys.argv) == 2 else "arguments"))
+if len(sys.argv) > 1:
+    for arg in range(1, len(sys.argv)):
+        print("{:d}: {:s}".format(arg, sys.argv[arg]))
