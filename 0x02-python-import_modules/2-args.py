b69fc113727e164c122fac04988d7bed3367345b
dhev-wisdom <nonsowisdom62@gmail.com>
2022-12-03 14:52:20 -0800

    check

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 import sys
-if len(sys.argv) == 2:
-    what_to_print = "argument."
-elif len(sys.argv) == 1:
-    what_to_print = "arguments."
-else:
-    what_to_print = "arguments:"
-print("{:d} {:s}".format(len(sys.argv) - 1, what_to_print))
-if len(sys.argv) > 1:
-    for arg in range(1, len(sys.argv)):
-        print("{:d}: {:s}".format(arg, sys.argv[arg]))
+def run():
+    if len(sys.argv) == 2:
+        what_to_print = "argument."
+    elif len(sys.argv) == 1:
+        what_to_print = "arguments."
+    else:
+        what_to_print = "arguments:"
+    print("{:d} {:s}".format(len(sys.argv) - 1, what_to_print))
+    if len(sys.argv) > 1:
+        for arg in range(1, len(sys.argv)):
+            print("{:d}: {:s}".format(arg, sys.argv[arg]))
+if __name__ = "__main__":
+    run()
