24015bbcced6dbd666b5bd7788e3a036a4c01966
dhev-wisdom <nonsowisdom62@gmail.com>
2022-12-03 11:13:45 -0800

    import module

---
+++ 0x02-python-import_modules/fibo.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def fib(n):
+    for i in range(1, n, 2):
+        print(i, end="")
+print()
+
+def fib2(n):
+    for j in range(2, n, 5):
+        print(j, end="")
+print()
---
+++ 0x02-python-import_modules/test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from fibo import fib, fib2
+fib(50)
+__name__ = "__main__"
+
+if __name__ == "__main__":
+    import sys
+    fib(int(sys.argv[1]))
