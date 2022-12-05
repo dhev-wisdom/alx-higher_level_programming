d2f40c54f779a7ed4eb05cba80215b546acfe686
dhev-wisdom <nonsowisdom62@gmail.com>
2022-12-05 12:41:22 -0800

    largest number

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    largest = 0
+    largest = None
     for num in my_list:
-        if largest <= num:
+        if largest == None or largest <= num:
             largest = num
     return largest
