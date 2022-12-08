25271436c2daf875c82475c88ae787a7051d6306
dhev-wisdom <nonsowisdom62@gmail.com>
2022-12-07 21:00:30 -0800

    Search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_l = []
+    for item in my_list:
+        if item == search:
+            new_l.append(replace)
+        else:
+            new_l.append(item)
+    return new_list
