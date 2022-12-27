6559ec684be38d334513f614d722b19ef705f3dd
dhev-wisdom <nonsowisdom62@gmail.com>
2022-12-27 00:19:42 -0800

    Check

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Module creates an emptt class, Square"""
 class Square:
     """Square is an empty class that does nothing"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 if __name__ = "__main__":
     class Square:
-        def __init__(self, size):
+        def __init__(self, size=0):
             self.__size = size
