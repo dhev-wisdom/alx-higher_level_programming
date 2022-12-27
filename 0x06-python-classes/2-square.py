d831a70379e811b5b617b11c923b609fe9c6ada1
dhev-wisdom <nonsowisdom62@gmail.com>
2022-12-27 07:12:51 -0800

    Check

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module creates an emptt class, Square"""
+
+
+
+class Square:
+    """Square is an empty class that does nothing"""
+    def __init__(self, size=0):
+        try:
+            if isinstance(size, int):
+                try:
+                    if size >= 0:
+                        self.__size = size
+                    else:
+                        raise ValueError
+                except ValueError:
+                    print("size must be >= 0")
+            else:
+                raise TypeError:
+        except TypeError:
+            print("size must be an integer")
