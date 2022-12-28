c9814c2138ce33bb0ef3759f929a6836faa55ec0
dhev-wisdom <nonsowisdom62@gmail.com>
2022-12-28 05:32:51 -0800

    Check

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+"""Module creates an empty class, Square"""
+
+
+class Square:
+    """Square is an empty class that does nothing"""
+    def __init__(self, size=0, position=(0,0)):
+        """init function initializes an instance of Square class"""
+        try:
+            if isinstance(size, int):
+                try:
+                    if size >= 0:
+                        self.__size = size
+                    else:
+                        raise ValueError("size must be >= 0")
+                except ValueError as err:
+                    print(err)
+            else:
+                raise TypeError("size must be an integer")
+        except TypeError as err:
+            print(err)
+
+        try:
+            (pos1, pos2) = position
+            if isinstance(pos1, int) & isinstance(pos2, int):
+                self.__position = (pos1, pos2)
+            else:
+                raise TypeError("position must be a tuple of 2 positive integers")
+        except TypeError as err:
+            print(err)
+
+    def area(self):
+        "area is a method of Square class that returns the area of the square"
+        x = self.__size
+        return x**2
+
+    @property
+    def size(self):
+        """getter method: returns value of size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter method: sets value of size to passed parameter"""
+        try:
+            if isinstance(value, int):
+                try:
+                    if value >= 0:
+                        self.__size = value
+                    else:
+                        raise ValueError("size must be >= 0")
+                except ValueError as err:
+                    print(err)
+            else:
+                raise TypeError("size must be an integer")
+        except TypeError as err:
+            print(err)
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, position):
+        (pos1, pos2) = position
+        self.__position = (pos1, pos2)
+
+    def my_print(self):
+        """Public instance method to print the square with '#'"""
+        x = self.__size
+        y = self.__position
+        if x > 0:
+            for i in range(x):
+                print("_" * y[0], end="")
+                for j in range(x):
+                    print("#", end="")
+                print()
+        else:
+            print()
