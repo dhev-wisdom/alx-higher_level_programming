415cbc27cec782335532d44cdb4f57c1e83e8be4
dhev-wisdom <nonsowisdom62@gmail.com>
2023-01-02 13:05:05 -0800

    Chexk

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Module creates and empty clasd, Rectangle"""
+
+
+class Rectangle:
+    """Empty class Rectangle"""
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Getter method that returns width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter method to set the value of width"""
+        try:
+            if isinstance(value, int):
+                try:
+                    if value >= 0:
+                        self.__width = value
+                    else:
+                        raise ValueError("width  must be >= 0")
+                except ValueError as err:
+                    print(err)
+            else:
+                raise TypeError("width must be an integer")
+        except TypeError as er:
+            print(er)
+
+    @property
+    def height(self):
+        """Getter method to return the value of height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter method to set the vakue of height"""
+        try:
+            if isinstance(value, int):
+                try:
+                    if value >= 0:
+                        self.__height = value
+                    else:
+                        raise ValueError("height must be >= 0")
+                except ValueError as err:
+                    print(err)
+            else:
+                raise TypeError("height must be an integer")
+        except TypeError as er:
+            print(er)
