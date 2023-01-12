615b297db049e190019cb6bc9ddbdce4249a84b0
dhev-wisdom <nonsowisdom62@gmail.com>
2023-01-12 14:21:58 -0800

    Check

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Program to return JSON representation of an object string"""
+
+
+def to_json_string(my_obj):
+    """Function to convert string to JSON and return result"""
+
+    return json.dumps(my_obj)
