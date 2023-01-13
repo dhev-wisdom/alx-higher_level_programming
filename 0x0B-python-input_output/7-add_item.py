91bf5f01c3c4013465978a88ecbd4671572d7190
dhev-wisdom <nonsowisdom62@gmail.com>
2023-01-12 18:11:04 -0800

    Xcheck

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""Program that adds all arguments to a list and save to file"""
-
-
-
+import sys
 save = __import__('5-save_to_json_file').save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
-import sys
+
+
+"""Add script argument to json"""
 
 
 try:
     lst = load("add_item.json")
-except:
+except FileNotFoundError:
     lst = []
 
 length = len(sys.argv)
