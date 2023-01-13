4446235951d891a03a3e3cdf4d6835da68d9a000
dhev-wisdom <nonsowisdom62@gmail.com>
2023-01-13 01:47:26 -0800

    Check

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 import sys
-save = __import__('5-save_to_json_file').save_to_json_file
-load = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 """Add script argument to json"""
 
 
 try:
-    lst = load("add_item.json")
+    lst = load_from_json_file("add_item.json")
 except FileNotFoundError:
     lst = []
 
@@ -16,4 +16,4 @@ length = len(sys.argv)
 if length > 1:
     for i in range(1, length):
         lst.append(sys.argv[i])
-save(lst, "add_item.json")
+save_to_json_file(lst, "add_item.json")
