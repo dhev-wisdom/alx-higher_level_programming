816a2698a1f2661723bb637d85e18dd86d2d0b79
dhev-wisdom <nonsowisdom62@gmail.com>
2023-01-13 01:49:43 -0800

    X

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,7 +9,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
     lst = load_from_json_file("add_item.json")
-except FileNotFoundError:
+except:
     lst = []
 
 length = len(sys.argv)
