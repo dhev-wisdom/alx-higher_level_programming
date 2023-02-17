ea3840e8c81822d4b491c7acb265f2629bfda4d0
dhev-wisdom <nonsowisdom62@gmail.com>
2023-02-17 03:07:13 -0800

    Create table

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,4 +1,6 @@
 -- Write a script that creates the table
 -- force_name on your MySQL server.
+-- id INT; name VARCHAR(256)
+-- If table exists, script shouldn't throw error
 CREATE TABLE IF NOT EXISTS force_table
 (id INT, name VARCHAR(256) NOT NULL);
