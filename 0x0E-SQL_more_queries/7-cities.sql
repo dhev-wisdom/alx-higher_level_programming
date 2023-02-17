54a503e125a71b912e32250549be9425f87f8922
dhev-wisdom <nonsowisdom62@gmail.com>
2023-02-17 03:35:57 -0800

    foreign key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- creates database and table
+-- table description: id INT UNIQUE AUTO-GENERATED NOT NULL PRIMARY KEY;
+-- ... name VARCHAR(256) NOT NULL
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+	id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	state_id INT NOT NULL,
+	FOREIGN KEY(state_id) REFERENCES states(id),
+	name VARCHAR(256) NOT NULL);
