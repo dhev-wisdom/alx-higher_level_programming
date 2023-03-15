2121b63c1d45f9532e8a72dce5089b4387f1d664
dhev-wisdom <nonsowisdom62@gmail.com>
2023-03-15 16:05:39 -0700

    Fix no 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -5,12 +5,10 @@ let arg2 = process.argv[3];
 if (isNaN(arg1) === false && isNaN(arg2) === false) {
 	arg1 = parseInt(arg1);
 	arg2 = parseInt(arg2);
-} else {
-	console.log("One of the arguments can't be added");
-	return;
 }
 
 function add(a, b) {
-	console.log(a + b);
+	console.log(Number(a) + Number(b));
 }
+
 add(arg1, arg2);
