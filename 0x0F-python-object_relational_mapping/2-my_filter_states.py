2ff025593eb8404989e6698b26b4440678c94b42
dhev-wisdom <nonsowisdom62@gmail.com>
2023-03-19 17:01:48 -0700

    task-2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Module that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument
+
+Arguments:
+    mysql username (str)
+    mysql password (str)
+    database name (str)
+    state name searched (str)
+"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    mysql_user = sys.argv[1]
+    mysql_pw = sys.argv[2]
+    mysql_db = sys.argv[3]
+    serached_name = sys.argv[4]
+
+    # By default, it will connect to localhost:3306
+    db = MySQLdb.connect("user=mysql_user, passwd=mysql_pw, db=mysql_db")
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id"
+                .format(searched_name))
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
