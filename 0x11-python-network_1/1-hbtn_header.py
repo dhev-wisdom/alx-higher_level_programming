1a7f518c0940dfc2eccda573f62e39091d7892ff
dhev-wisdom <nonsowisdom62@gmail.com>
2023-03-31 16:25:08 -0700

    getheader

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,7 +8,11 @@ import urllib.request
 import sys
 
 
-url = sys.argv[1]
-req = urllib.request.Request(url)
-with urllib.request.urlopen(req) as response:
-    print(response.getheader('X-Request-Id'))
+def _import():
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        print(response.getheader('X-Request-Id'))
+
+if __name__ == "__main__":
+    _import()
