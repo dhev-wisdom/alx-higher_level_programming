3b68d28b3b98eb7ff7209b1b6b3b0289a82003c2
dhev-wisdom <nonsowisdom62@gmail.com>
2023-03-31 16:06:49 -0700

    Check

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" 
+    cript that takes in a URL, sends a request to the URL and displays the value of
+    the X-Request-Id variable found in the header of the response
+"""
+
+from urllib.request import  urlopen, Request
+from urllib.parse import urlencode
+import sys
+
+
+
+url = sys.argv[1]
+req = Request(url)
+with urlopen(req) as response:
+    print(response.info.X-Request-Id)
