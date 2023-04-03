1d88dfff6a0c13564712f7595b0f74a163fc9d5e
dhev-wisdom <nonsowisdom62@gmail.com>
2023-04-04 00:24:17 +0100

    Check

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a JSON via POST request using cURL
-curl -X POST -H "Content-type: application/json" -d "@${json}" "$url" <<< IFS='/' read -r url json <<< "$1"
+IFS='/' read -r url json <<< "$1" && curl -X POST -H "Content-type: application/json" -d "@${json}" "$url"
