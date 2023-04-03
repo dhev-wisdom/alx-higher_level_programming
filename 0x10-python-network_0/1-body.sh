cdbae5be30a82e8e06eb4bfbdcd3d4d6a5228adb
dhev-wisdom <nonsowisdom62@gmail.com>
2023-04-03 19:29:32 +0100

    Check curl

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script sends a GET request to the URL, and displays the body of the response
-curl -s -o /dev/null -w '%{http_code}' $1 | awk '{ if ($http_code==200) print }'
+status_code=$(curl -s -o /dev/null -w "%{http_code}" "$1") && if [ $status_code == 200 ]; then curl -s $1; fi
