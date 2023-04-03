e5f2aa205b9f0954758aa2d035adb16172843647
dhev-wisdom <nonsowisdom62@gmail.com>
2023-04-04 00:27:51 +0100

    POST

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a JSON via POST request using cURL
-IFS='/' read -r url json <<< "$1" && curl -X POST -H "Content-type: application/json" -d "@${json}" "$url"
+curl -X POST -H "Content-type: application/json" -d $2 $1
