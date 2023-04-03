00e386736a703ed6573e788b45ac787c34dc6679
dhev-wisdom <nonsowisdom62@gmail.com>
2023-04-03 19:02:14 +0100

    chek

--- 0x10-python-network_0/fret.sh
+++ 0x10-python-network_0/fret.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # script sends a GET request to the URL, and displays the body of the response
+# curl -s -o /dev/null -w '%{http_code}' $1 | awk {'if($0==200) {getline; print}}'
+# status_code=$(curl -s -o /dev/null -w "%{http_code}" "$1") && if [ $status_code==200 ] then curl $1 fi
 status_code=$(curl -s -o /dev/null -w "%{http_code}" "$1") && [[ $status_code == 200 ]] && curl -s $1
