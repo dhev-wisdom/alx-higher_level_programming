5bb76e8d860b81b8125586bc97712ca9e57b6c3f
dhev-wisdom <nonsowisdom62@gmail.com>
2023-04-03 14:16:13 +0100

    Check curl

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/bash
-# Script takes a URL
-# Sends a request to the URL
-# and displays the size of the body of the response
-
-url=$1
-# size=$(curl -s -w "%{size_download}" "$url")
-size=$(curl -s -o /dev/null -w "%{size_download}" "$url")
-echo $size
+# Script takes a URL, sends a request and displays the size of the body
+echo $(curl -s -w "%{size_download}" "$1")
