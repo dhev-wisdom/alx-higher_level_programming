fa276d26a2602704902c2949f5da0cbaefc2e1af
dhev-wisdom <nonsowisdom62@gmail.com>
2023-04-04 01:05:01 +0100

    Check

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script displays all available HTTP methods for server using `curl`
-curl -s -X OPTIONS -I $1 | grep "Allow" |  tr -d '\r' | awk '{print $2}'
+curl -s -X OPTIONS -I $1
