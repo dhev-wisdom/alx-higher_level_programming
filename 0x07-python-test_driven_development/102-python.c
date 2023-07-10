bb1b5bac24d775b5d96177e17bc60aee1b33eeb6
dhev-wisdom <nonsowisdom@gmail.com>
2023-07-09 17:05:00 -0700

    c

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,28 +1,11 @@
 #include <Python.h>
 
 void print_python_string(PyObject *p) {
-    if (!PyUnicode_Check(p)) {
-        printf("Error: Invalid string object\n");
-        return;
-    }
+  if (!PyString_Check(p)) {
+    printf("p is not a valid string\n");
+    return;
+  }
 
-    Py_ssize_t length = PyUnicode_GET_LENGTH(p);
-    Py_UCS4 *data = PyUnicode_AsUCS4Copy(p);
-
-    if (!data) {
-        printf("Error: Failed to get string data\n");
-        return;
-    }
-
-    printf("String length: %zd\n", length);
-    printf("String content: ");
-
-    for (Py_ssize_t i = 0; i < length; i++) {
-        printf("%c", data[i]);
-    }
-
-    printf("\n");
-
-    PyMem_Free(data);
+  char *s = PyString_AsString(p);
+  printf("%s\n", s);
 }
-
