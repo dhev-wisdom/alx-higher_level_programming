3f97a6df63ccf440486897233c2f31247e5cb2d7
dhev-wisdom <nonsowisdom@gmail.com>
2023-07-14 15:30:10 -0700

    raise Exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module defines an empty class 'BaseGeometry'
+"""
+
+
+class BaseGeometry():
+    """
+    an empty class
+    """
+
+    def area(self):
+        """
+        public method raises an Exception
+        """
+        raise Exception("area() is not implemented")
