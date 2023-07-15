b92e46d1ad851105405f8a90c45cfb4dfbc834f9
dhev-wisdom <nonsowisdom@gmail.com>
2023-07-14 17:23:42 -0700

    ipdate

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -117,6 +117,21 @@ class Rectangle(Base):
                 print("#", end="")
             print("")
 
+    def update(self, *args):
+        """
+        Method to update attributes
+        """
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
+
     def __str__(self):
         """
         the standard str function that defines what a class prints
